5ded0c2eafb6dd113aaf22460372f884e2e6a4c4
waveinyu <waveinyu@gmail.com>
2024-01-11 19:43:17 +0900

    Add: property 프로퍼티 감시자

--- on-playground.playground/Contents.swift
+++ on-playground.playground/Contents.swift
@@ -61,3 +61,22 @@ yuyu.name = "유영"
 print(yuyu.name)
 print(yuyu.selfIntroduce)
 print("제 한국 나이는 \(yuyu.koreanAge)이고 미국 나이는 \(yuyu.westernAge)살입니다")
+
+
+// 값이 변경 되기 전 로그를 찍거나, 데이터베이스 저장을 하는 등의 작업에 사용
+struct Money {
+    // 프로퍼티 감시자 사용
+    var currencyRate: Double = 1100 {
+        willSet(newRate) {
+            print("환율이 \(currencyRate)에서 \(newRate)로 변경될 예정")
+        }
+        
+        didSet(oldRate) {
+            print("환율이 \(oldRate)에서 \(currencyRate)로 변경됨")
+        }
+    }
+}
+
+
+var money: Money = Money()
+money.currencyRate = 1150
