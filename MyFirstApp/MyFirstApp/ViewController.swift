3b0c623fa8f715dff4a0a08b77e4509038f0aa12
waveinyu <waveinyu@gmail.com>
2024-01-17 17:55:37 +0900

    Init: Initialize project with basic structure

---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 유영 on 1/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
