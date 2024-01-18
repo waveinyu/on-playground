d9c767582c13b8c46323ada736d9e374c12a92d7
waveinyu <waveinyu@gmail.com>
2024-01-18 16:34:17 +0900

    Feat: `mainLable`, `pressedButton`아웃렛 연결 및 액션 추가

--- MyFirstApp/MyFirstApp/ViewController.swift
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
+// 화면 하나 당 관리하는 코드
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainLable: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func pressedButton(_ sender: UIButton) {
+        
+        /*
+         mainLabel.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+         */
+        
+        mainLable.text = "월드"
+        mainLable.backgroundColor = UIColor.gray
+        mainLable.textColor = UIColor.white
+                
+        
+    }
+    
 }
-
+ 
